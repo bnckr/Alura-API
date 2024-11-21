17a4fcd34e8dce7ae66361a6535cded36f91fe6e
Bianca <bnckr@outlook.com>
2024-11-21 16:21:37 -0300

    fix: 🐛

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
 import { getTodosPosts, criarPost } from "../models/postsModel.js";
+import fs from "fs";
 
 export async function listarPosts(req, res) {
   const posts = await getTodosPosts();
@@ -17,9 +18,16 @@ export async function postarNovoPost(req, res) {
 }
 
 export async function uploadImagem(req, res) {
-  const novoPost = req.body;
+  const novoPost = {
+    descricao: "",
+    imgUrl: req.file.originalname,
+    alt: "",
+  };
+
   try {
     const postCriado = await criarPost(novoPost);
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.jpg`;
+    fs.renameSync(req.file.path, imagemAtualizada);
     res.status(200).json(postCriado);
   } catch (erro) {
     console.error(erro.message);
